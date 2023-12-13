984e2859e319b4ec204a133114913eb6db33b34d
任奇 <87684714+renqi1@users.noreply.github.com>
2023-12-13 13:21:10 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -324,7 +324,7 @@ def parse_opt():
     parser.add_argument('--source', type=str, default='data/image', help='source')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.2, help='NMS IoU threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='show results')
